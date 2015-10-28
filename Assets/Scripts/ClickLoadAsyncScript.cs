a7c56f7d64059bbd49e2a1bc33a5bff976c307b4
CardStrife <rpg.95cergy@gmail.com>
2015-10-28 07:00:06 +0100

    Ajout des dossiers

---
+++ Assets/Scripts/ClickLoadAsyncScript.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class ClickLoadAsyncScript : MonoBehaviour {
+
+	public Slider loadingSlider;
+	public GameObject loadingImage;
+	
+	private AsyncOperation async;
+	
+	public void ClickAsync(int level)
+	{
+		loadingImage.SetActive(true);
+		StartCoroutine(loadLevelWithBar(level));
+	}
+	
+	IEnumerator loadLevelWithBar (int level)
+	{
+		async = Application.LoadLevelAsync(level);
+		while(!async.isDone)
+		{
+			loadingSlider.value = async.progress;
+			yield return null;
+		}
+	}
+}
---
+++ Assets/Scripts/SelectionChapterButtonScript.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SelectionChapterButtonScript : MonoBehaviour {
+
+	public GameObject loadingImage;
+
+	public void LoadChapter()
+	{
+		loadingImage.SetActive(true);
+		Application.LoadLevel("SelectionChapter");
+	}
+}
